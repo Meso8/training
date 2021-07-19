1ca135df5183eca057be2108b3198427736813b1
Meso8 <86187391+Meso8@users.noreply.github.com>
2021-07-19 14:50:32 +0300

    print function with escape characters

---
+++ print_hello_intro.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("I'm new to this so bare with me ")
+print('I\'m learning Python step by step')
+print("Starting with" + "print function")
+print("learing about escape characters such as \\ which will skip the character behind it and another escape character is \\ with t behind which symbolize space as wide as a tab ")
+print("another escape characters is \\ with n which means the beginning of a new line.")
+print("""
+\"\"\"  three quotation marks refrence an open space with no limits unitl it closed with same marks 
+""")
